d05e208b1305a4baeb8ff309f6a8466ab5ba5604
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-31 15:51:59 +0530

    [Sparsh] Add: Backend Setup Completed

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express=require("express");
+const cors=require("cors");
+const path=require("path");
+const connectDB = require("./config/db");
+const PORT=process.env.PORT || 50000;
+
+const app=express();
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    })
+);
+
+app.use(express.json());
+
+connectDB();
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/tasks", taskRoutes);
+// app.use("/api/reports", reportRoutes);
+
+app.listen(PORT, ()=>console.log(`Server Running on PORT: ${PORT}`));
